b8cc3456fd817e011c777b88fab381cc65cd4bad
Dennis MacConnell <dennis.macconnell@oracle.com>
2018-04-05 07:50:56 -0400

    added hello world

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+clean:
+	mvn clean
+
+package:
+	mvn package
+
+hello:
+	java -jar target/java-examples-1.0-SNAPSHOT.jar
---
+++ src/main/java/com/ispycode/examples/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.ispycode.examples;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(args.length);
+    }
+}
